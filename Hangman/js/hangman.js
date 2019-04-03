2a9b32a4b224ca07d718bab80b1e0eac6c7fc492
schang154 <shchang1128@gmail.com>
2019-04-02 17:17:41 -0700

    update

--- Hangman/js/hangman.js
+++ Hangman/js/hangman.js
@@ -1,8 +1,8 @@
 let wordArray = ["TATTOO", "COMMITTEE", "ELECTRICITY", "COMPUTER", "VACUUM", "AMIR", "MEME", "JAVASCRIPT", "PYTHON", "BCIT"]; // 10 words to be chosen
 let definitions = [
-"mark (a person or a part of the body) with an indelible design by inserting pigment into punctures in the skin", 
+"a form of body modification where a design is made by inserting ink", 
 "a group of people appointed for a specific function, typically consisting of members of a larger group", 
-"a form of energy resulting from the existence of charged particles",
+"electricity, is the set of physical phenomena associated with the presence and motion of electric charge",
 "an electronic device for storing and processing data, typically in binary form, according to instructions given to it in a variable program",
 "an electrical apparatus that by means of suction collects dust and small particles from floors and other surfaces",
 "the best teacher",
@@ -17,6 +17,7 @@ let displayString;
 let lives = 7;
 let score = 0;
 let count = 0;
+let playerList = [];
 
 document.getElementById("lives").innerHTML = "Lives: " + lives;
 document.getElementById("score").innerHTML = "Score: " + score;
@@ -33,7 +34,7 @@ function chooseWord() {
 }
 
 function check(letter, word) {
-    let count = 0
+    let count = 0;
     for (let i = 0; i < word.length; i++) {
         if (word.charAt(i) == letter) {
             displayWordArray[i] = letter;
@@ -75,7 +76,7 @@ function generateButtons() {
 }
 
 function displayScore() {
-    name = prompt('Enter your name:');
+    let name = prompt('Enter your name:');
     document.getElementById('display').innerHTML = name + ', your score is ' + score;
 }
 
@@ -83,6 +84,8 @@ function gameStatus(chosenWord) {
     if (lives == 0) {
         document.getElementById("gameover").innerHTML = "You died! Game over.";
         displayScore();
+        writeLeaderBoard(name, score);
+        getLeaderBoard();
     }
     if (chosenWord == displayString) {
         document.getElementById("gameover").innerHTML = "You win!";
@@ -90,7 +93,9 @@ function gameStatus(chosenWord) {
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].disabled = true;
         }
-        setTimeout(displayScore, 0);
+        displayScore();
+        writeLeaderBoard(name, score);
+        getLeaderBoard();   
     }
 }
 
@@ -109,8 +114,54 @@ function restartGame() {
     document.getElementById("lives").innerHTML = "Lives: " + lives;
     document.getElementById("score").innerHTML = "Score: " + score;
     document.getElementById('guessString').innerHTML = displayString;
+    hideScore();
+}
+
+function writeLeaderBoard(name, score) {
+    let dbRef = firebase.database().ref("users/");
+    dbRef.update({
+        [name]: score
+    })
+  }
+
+function getLeaderBoard() {
+    let dbRef = firebase.database().ref("users/");
+    dbRef.on("value", function (snapshot) {
+        let leaderBoard = snapshot.val();
+        console.log(leaderBoard);
+        sortLeaderBoard(leaderBoard);
+    })
+}
+
+function sortLeaderBoard(leaderBoard){
+    for (name in leaderBoard){
+        playerList.push([name, leaderBoard[name]])
+    }
+    playerList.sort(function(key2, key1){
+        return key1[1]-key2[1];
+    })
+    displayLeaderBoard(playerList);
+
+}
+
+function displayLeaderBoard(playerList){
+    let parent = document.createElement('div');
+    parent.setAttribute('id', 'leaderboard');
+    document.body.appendChild(parent);
+    for (let i=0; i<playerList.length; i++){
+        // console.log(playerList[i][0] + ' ' + playerList[i][1]);
+        let node = document.createElement('div');
+        node.innerHTML = playerList[i][0] + ' ' + playerList[i][1];
+        parent.appendChild(node);   
+        }        
+    }
+
+function hideScore(){
+    document.getElementById('leaderboard').remove();
+    firebase.database().ref("users/").off();
+    playerList = []
 }
 
 chooseWord();
-document.getElementById('guessString').innerHTML = displayString
+document.getElementById('guessString').innerHTML = displayString;
 generateButtons();
