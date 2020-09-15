2a889b2a88d1bbfb2c0b1d4b434c7f422d201feb
schang154 <shchang1128@gmail.com>
2020-09-15 01:44:37 -0700

    new index page for comp4537

--- Stranded_game/js_dir/authentication.js
+++ Stranded_game/js_dir/authentication.js
@@ -23,16 +23,16 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'index.html',
+    signInSuccessUrl: 'new_index.css.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 
     ],
     // Terms of service url.
-    tosUrl: 'index.html',
+    tosUrl: 'new_index.css.html',
     // Privacy policy url.
-    privacyPolicyUrl: 'index.html'
+    privacyPolicyUrl: 'new_index.css.html'
 };
 
 // The start method will wait until the DOM is loaded.
